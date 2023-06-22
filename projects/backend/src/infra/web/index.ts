e6228faa308f7764e864f9abf5a02be8647d03cb
Wesley Alves <wesley_alv@outlook.com>
2023-06-21 21:34:22 -0300

    refactor: config middleware de segurança no servidor http

--- projects/backend/src/infra/web/index.ts
+++ projects/backend/src/infra/web/index.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core'
+import helmet from 'helmet'
 
 import { AppModule } from './app.module'
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule)
+  app.use(helmet())
   await app.listen(3000)
 }
 
