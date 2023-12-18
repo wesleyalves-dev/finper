2ae6f1b785e2f28ec3313d9fa58ee9c9891a6eb4
Wesley Alves <wesley_alv@outlook.com>
2023-12-18 14:42:15 -0300

    chore: back-end configurado

---
+++ projects/backend/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: ['standard-with-typescript', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json'
+  },
+  rules: {},
+  ignorePatterns: ['.eslintrc.js']
+}
